815215a605951797a461be026de5119719d98d5d
suraj <suraj.tech.in@gmail.com>
2025-05-24 19:01:32 +0545

    Added UI library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/templates/**/*.html"],
+  content: [
+    "./app/templates/**/*.html",
+    "./node_modules/flowbite/**/*.js",
+    "./node_modules/preline/**/*.js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
