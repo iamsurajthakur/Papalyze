5d4cc0558a699150cff1a9b28f02f8c8f95914ca
suraj <suraj.tech.in@gmail.com>
2025-05-26 21:40:11 +0545

    Add basic homepage layout and initial routes

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -3,5 +3,17 @@ from app.blueprints.main import bp
 
 @bp.route('/')
 def index():
-    return render_template('partials/navbar_guest.html', title='Home')
+    return render_template('main/index.html', title='Home')
+
+@bp.route('/contact.html')
+def contact():
+    return render_template('main/contact.html', title='Contact')
+
+@bp.route('/about.html')
+def about():
+    return render_template('main/about.html', title='About')
+
+@bp.route('/login.html')
+def login():
+    return render_template('main/login.html', show_navbar=False)
 
